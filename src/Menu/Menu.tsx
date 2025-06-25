2e440052b39e41793271368fb87d5bc607a18b0c
nk-on <2nkoz@proton.me>
2025-06-24 21:53:45 -0700

    adding interface to the Menu component

--- src/Menu/Menu.tsx
+++ src/Menu/Menu.tsx
@@ -1,13 +1,14 @@
 import MenuItem from "./MenuItem";
 import { menuItems } from "./menuItems";
 import { useState } from "react";
+interface MenuProps {
+  dispatch: React.ActionDispatch<[action: { type: string }]>;
+  setBarPercentage: React.Dispatch<React.SetStateAction<number>>;
+}
 export default function Menu({
   dispatch,
   setBarPercentage,
-}: {
-  dispatch: React.ActionDispatch<[action: { type: string }]>;
-  setBarPercentage: React.Dispatch<React.SetStateAction<number>>;
-}) {
+}: MenuProps) {
   const [selectedId, setSelectedId] = useState<number>(-1);
   return (
     <div className="lg:w-[35vw] w-[90vw] h-[56px] bg-[#151932] rounded-[25px] flex justify-between items-center px-[10px]">
