de494fc2fbcdd51a274364597b0934b0f4b49ea1
nk-on <2nkoz@proton.me>
2025-06-26 21:32:27 -0700

    Fix:fixing bug in particular app was not able to switch between modes i fixed this issue

---
+++ src/Reducer.ts
@@ -0,0 +1,20 @@
+import { initialStates } from "./initialStates";
+import type { TimerState } from "./Interfaces";
+import type { TimerAction } from "./Interfaces";
+export default function reducer(
+  state: TimerState | undefined,
+  action: TimerAction
+): TimerState | undefined {
+  const mode = action.type;
+  const resObj = initialStates.find((element) => element.mode === mode)?.state;
+  if (action.type === "RESTART") {
+    return resObj;
+  }
+  if (action.type === "seconds decrease" && state) {
+    return { ...state, seconds: state.seconds - 1 };
+  }
+  if (action.type === "minutes decrease" && state) {
+    return { seconds: 59, minutes: state.minutes - 1 };
+  }
+  return resObj
+}
