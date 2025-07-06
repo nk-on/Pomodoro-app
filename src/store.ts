e2be81d1407673cb4ca6d7722d7c12b636f56348
nk-on <2nkoz@proton.me>
2025-07-05 22:15:00 -0700

    Fix:fixing the bug in particular seconds continued decreasing even after they passed bellow 0 instead i set it on 59

--- src/store.ts
+++ src/store.ts
@@ -2,7 +2,7 @@ import { create } from "zustand";
 export const useStore = create((set) => ({
     timerState: { minutes: 24, seconds: 59 },
     secondsDecrease: () => set((state)=> ({timerState:{...state.timerState, seconds: state.timerState.seconds - 1 }})),
-    minutesDecrease: () =>  set((state) => ({ timerState:{...state.timerState, minutes: state.timerState.minutes - 1 }})),
+    minutesDecrease: () =>  set((state) => ({ timerState:{minutes: state.timerState.minutes - 1, seconds:59}})),
     modeSwitch:(mode)=> {
        if(mode.current === "Short break"){
             return set(()=>({timerState:{ minutes: 4, seconds: 59}}))
