b5d7d276e9367bd3ea04819a259d15aa95780aca
nk-on <2nkoz@proton.me>
2025-07-05 22:54:59 -0700

    Fixing typing issues regarding useStore

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -1,31 +1,44 @@
 export interface TimerState {
-    minutes: number;
-    seconds: number;
+  minutes: number;
+  seconds: number;
 }
 
 export type TimerAction =
-    | { type: "Pomodoro" }
-    | { type: "Short break" }
-    | { type: "seconds decrease" }
-    | { type: "minutes decrease" }
-    | { type: string };
+  | { type: "Pomodoro" }
+  | { type: "Short break" }
+  | { type: "seconds decrease" }
+  | { type: "minutes decrease" }
+  | { type: string };
 export interface Clock {
   minutes: number;
   seconds: number;
   dispatch: React.ActionDispatch<[action: { type: string }]>;
-  barPercentage:number,
-  setBarPercentage:React.Dispatch<React.SetStateAction<number>>,
-  mode:React.RefObject<string>
+  barPercentage: number,
+  setBarPercentage: React.Dispatch<React.SetStateAction<number>>,
+  mode: React.RefObject<string>
 }
 export interface MyComponentProps {
   title: string;
   id: number;
   selectedId: number;
   setSelectedId: React.Dispatch<React.SetStateAction<number>>;
-  dispatch: React.ActionDispatch<[action: { type: string }]>;
   setBarPercentage: React.Dispatch<React.SetStateAction<number>>;
-  mode:React.RefObject<string>
+  mode: React.RefObject<string>
 }
 export interface TimerProps {
-  setSettingsVisible:React.Dispatch<React.SetStateAction<boolean>>
+  setSettingsVisible: React.Dispatch<React.SetStateAction<boolean>>;
+}
+export interface TimeInputProps {
+  title: string,
+  timerMode:number,
+  setFunction: React.Dispatch<React.SetStateAction<number>>;
+}
+export interface TimerState {
+    minutes:number ; seconds:number
+}
+export interface StoreType {
+  timerState: TimerState;
+  secondsDecrease: () => void;
+  minutesDecrease: () => void;
+  modeSwitch: (mode: { current: string }) => void;
 }
--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,10 @@
 import { create } from "zustand";
-export const useStore = create((set) => ({
+import type {StoreType}  from "./Interfaces";
+export const useStore = create<StoreType>((set) => ({
     timerState: { minutes: 24, seconds: 59 },
     secondsDecrease: () => set((state)=> ({timerState:{...state.timerState, seconds: state.timerState.seconds - 1 }})),
     minutesDecrease: () =>  set((state) => ({ timerState:{minutes: state.timerState.minutes - 1, seconds:59}})),
-    modeSwitch:(mode)=> {
+    modeSwitch:(mode:{current:string})=> {
        if(mode.current === "Short break"){
             return set(()=>({timerState:{ minutes: 4, seconds: 59}}))
         }
