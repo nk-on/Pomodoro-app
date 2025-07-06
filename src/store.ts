3c8d32d4915ce7ba5d46513185968dd223d470fa
nk-on <2nkoz@proton.me>
2025-07-05 22:10:27 -0700

    Deleting unused variables

--- src/store.ts
+++ src/store.ts
@@ -5,11 +5,11 @@ export const useStore = create((set) => ({
     minutesDecrease: () =>  set((state) => ({ timerState:{...state.timerState, minutes: state.timerState.minutes - 1 }})),
     modeSwitch:(mode)=> {
        if(mode.current === "Short break"){
-            return set((state)=>({timerState:{ minutes: 4, seconds: 59}}))
+            return set(()=>({timerState:{ minutes: 4, seconds: 59}}))
         }
         if(mode.current === "Long break"){
-            return set((state)=>({timerState:{minutes:8,seconds:59}}))
+            return set(()=>({timerState:{minutes:8,seconds:59}}))
         }
-        return set((state)=> ({timerState:{minutes:24,seconds:59}}))
+        return set(()=> ({timerState:{minutes:24,seconds:59}}))
     }
 }))
