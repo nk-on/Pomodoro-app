50e5b3845e05a6dc1df3bbb14c236556cde411b4
nk-on <2nkoz@proton.me>
2025-07-12 16:44:25 -0700

    Fix:fixed the bug in partuclar user was not able to change font family

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,21 @@ import { useState } from "react";
 import { useStore } from "./store";
 function App() {
   const [settingsVisible, setSettingsVisible] = useState<boolean>(false);
-  const font = useStore((state)=>state.font);
-  console.log(font)
+  const font = useStore((state) => state.font);
+  const fontClass = {
+    kumbh: "font-kumbh",
+    roboto: "font-roboto",
+    mono: "font-mono",
+  }[font];
   return (
     <>
-      <div className={`bg-[#1f213f] w-[100vw] h-[100vh] flex flex-col gap-[20px] justify-center items-center font-${font}`}>
+      <div
+        className={`bg-[#1f213f] w-[100vw] h-[100vh] flex flex-col gap-[20px] justify-center items-center ${fontClass}`}
+      >
         {settingsVisible ? (
-          <Settings
-            setSettingsVisible={setSettingsVisible}
-          />
+          <Settings setSettingsVisible={setSettingsVisible} />
         ) : (
-          <Timer
-            setSettingsVisible={setSettingsVisible}
-          />
+          <Timer setSettingsVisible={setSettingsVisible} />
         )}
       </div>
     </>
