b6bb7861f360cb6a590da71384d1de814f0cf85b
nk-on <johndoe@example.com>
2025-07-15 18:12:16 +0400

    Feat:implementing feature which let's user to change input values inside time inputs with up and down arrows

--- src/Settings/TimeInput.tsx
+++ src/Settings/TimeInput.tsx
@@ -1,32 +1,59 @@
-import type { TimeInputProps } from "../Interfaces";
+import { useState } from 'react';
+import type { TimeInputProps } from '../Interfaces';
 function TimeInput({ title, customvaluesObj }: TimeInputProps) {
+  const [value, setValue] = useState<string | number>('');
+  console.log(value);
   return (
     <>
       <div className="w-[100%]">
         <div className="lg:w-[140px] h-[48px] bg-[#EFF1FA] rounded-[10px] flex justify-between items-center px-[10px]">
           <input
             type="number"
+            value={value}
             className="h-[48px] w-[50%]  bg-[#EFF1FA] outline-none"
-            onKeyDown={(e)=>{
-              if(["-", "e", "E", "+", "."].includes(e.key)){
+            onKeyDown={(e) => {
+              if (['-', 'e', 'E', '+', '.'].includes(e.key)) {
                 e.preventDefault();
               }
             }}
             onChange={(e) => {
-              customvaluesObj.current = customvaluesObj.current.map((element) => {
-                if (title === element.title) {
-                  return {
-                    ...element,
-                    minutes: Number(e.target.value),
-                  };
+              setValue(Number(e.target.value));
+              customvaluesObj.current = customvaluesObj.current.map(
+                (element) => {
+                  if (title === element.title) {
+                    return {
+                      ...element,
+                      minutes: Number(e.target.value),
+                    };
+                  }
+                  return element;
                 }
-                return element;
-              });
+              );
             }}
           />
           <div className="flex flex-col  pt-[10px]">
-            <div className="bg-[url(public/arrow-up.svg)] bg-no-repeat w-[12px] h-[17px] cursor-pointer"></div>
-            <div className="bg-[url(public/arrow-down.svg)] bg-no-repeat w-[12px] h-[17px] cursor-pointer"></div>
+            <div
+              className="bg-[url(public/arrow-up.svg)] bg-no-repeat w-[12px] h-[17px] cursor-pointer"
+              onClick={() =>
+                setValue((value) => {
+                  if (value === '') {
+                    return 1;
+                  }
+                  return Number(value) + 1;
+                })
+              }
+            ></div>
+            <div
+              className="bg-[url(public/arrow-down.svg)] bg-no-repeat w-[12px] h-[17px] cursor-pointer"
+              onClick={() =>
+                setValue((value) => {
+                  if (value === '' || Number(value) <= 1) {
+                    return 1;
+                  }
+                  return Number(value) - 1;
+                })
+              }
+            ></div>
           </div>
         </div>
       </div>
