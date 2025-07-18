e66b21d65b2c711a418a01db9b8d1ceb68994aab
nk-on <2nkoz@proton.me>
2025-07-17 21:42:13 -0700

    Fix:fixed the bug app was setting one minute more as a custom time then necessary

--- src/store.ts
+++ src/store.ts
@@ -1,35 +1,36 @@
 import { create } from "zustand";
-import type {StoreType}  from "./Interfaces";
-let [pomodoroMinutes,shortBreakMinutes,longBreakMinutes] = [24,4,9]
+import type { StoreType } from "./Interfaces";
+let [pomodoroMinutes, shortBreakMinutes, longBreakMinutes] = [24, 4, 9]
 export const useStore = create<StoreType>((set) => ({
-    timerState: { minutes: pomodoroMinutes, seconds: 59},
-    mode:'',
-    font:'kumbh',
-    mainColor:'#F87070 ',
-    secondsDecrease: () => set((state)=> ({timerState:{...state.timerState, seconds: state.timerState.seconds - 1 }})),
-    minutesDecrease: () =>  set((state) => ({ timerState:{minutes: state.timerState.minutes - 1, seconds:59}})),
-    modeSwitch:(mode:string)=> {
-       if(mode === "Short break"){
-            return set(()=>({timerState:{ minutes: shortBreakMinutes, seconds: 59}}))
+    timerState: { minutes: pomodoroMinutes, seconds: 59 },
+    mode: '',
+    font: 'kumbh',
+    mainColor: '#F87070 ',
+    secondsDecrease: () => set((state) => ({ timerState: { ...state.timerState, seconds: state.timerState.seconds - 1 } })),
+    minutesDecrease: () => set((state) => ({ timerState: { minutes: state.timerState.minutes - 1, seconds: 59 } })),
+    restartTimer: () => set(() => ({ timerState: { minutes: pomodoroMinutes, seconds: 59 } })),
+    modeSwitch: (mode: string) => {
+        if (mode === "Short break") {
+            return set(() => ({ timerState: { minutes: shortBreakMinutes, seconds: 59 } }))
         }
-        if(mode === "Long break"){
-            return set(()=>({timerState:{minutes:longBreakMinutes,seconds:59}}))
+        if (mode === "Long break") {
+            return set(() => ({ timerState: { minutes: longBreakMinutes, seconds: 59 } }))
         }
-        return set(()=> ({timerState:{minutes:pomodoroMinutes,seconds:59}}))
+        return set(() => ({ timerState: { minutes: pomodoroMinutes, seconds: 59 } }))
     },
-    setMode:(clickedMode)=>{
-        return set(()=> ({mode:clickedMode}))
+    setMode: (clickedMode) => {
+        return set(() => ({ mode: clickedMode }))
     },
-    customTime(minutesPomodoro,minutesShortBreak,minutesLongBreak){
-         pomodoroMinutes = minutesPomodoro;
-         shortBreakMinutes = minutesShortBreak;
-         longBreakMinutes = minutesLongBreak;
-         return set((state)=> ({timerState:{...state.timerState, minutes:minutesPomodoro}}))
+    customTime(minutesPomodoro, minutesShortBreak, minutesLongBreak) {
+        pomodoroMinutes = minutesPomodoro - 1;
+        shortBreakMinutes = minutesShortBreak - 1;
+        longBreakMinutes = minutesLongBreak - 1;
+        return set(() => ({ timerState: {minutes: minutesPomodoro - 1,seconds:59} }))
     },
-    setFont(selectedFont:string){
-        return set(()=> ({font:selectedFont}))
+    setFont(selectedFont: string) {
+        return set(() => ({ font: selectedFont }))
     },
-    setColor(selectedColor:string){
-        return set(()=> ({mainColor:selectedColor}))
-    }
+    setColor(selectedColor: string) {
+        return set(() => ({ mainColor: selectedColor }))
+    },
 }));
