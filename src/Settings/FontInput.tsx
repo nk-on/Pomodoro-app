7c807a69094673fd32413ff83a2b1584415d4175
nk-on <johndoe@example.com>
2025-07-19 22:08:51 +0400

    Deleting unnecessary import

--- src/Settings/FontInput.tsx
+++ src/Settings/FontInput.tsx
@@ -1,19 +1,20 @@
-import React from "react";
-import { useStore } from "../store";
-import type { FontInputProps } from "../Interfaces";
+import { useStore } from '../store';
+import type { FontInputProps } from '../Interfaces';
 export default function FontInput({
   font,
   id,
   selectedId,
   dispatch,
 }: FontInputProps) {
-  const setFont = useStore((state)=> state.setFont)
+  const setFont = useStore((state) => state.setFont);
   return (
     <div
-      className={`w-[40px] h-[40px] ${selectedId === id ? 'text-[#fff] bg-[#000]': 'bg-[#e3e4ed]'} rounded-[50%] flex justify-center items-center font-${font}`}
-      onClick={()=>{
-        dispatch({type:'fontInput',payload:{id}})
-        setFont(font)
+      className={`w-[40px] h-[40px] ${
+        selectedId === id ? 'text-[#fff] bg-[#000]' : 'bg-[#e3e4ed]'
+      } rounded-[50%] flex justify-center items-center font-${font}`}
+      onClick={() => {
+        dispatch({ type: 'fontInput', payload: { id } });
+        setFont(font);
       }}
     >
       Aa
